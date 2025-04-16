ec0a296aeb29c12594db1121f7beb86826568c50
LucasDanielClavero <l.clavero@alumno.um.edu.ar>
2025-04-16 02:13:10 -0300

    Fase 2: Implementación de la función is_palindrome

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,3 +1,12 @@
 def is_palindrome(texto):
+    texto_limpio = ""
+    contador = 0
+
+    while contador < len(texto):
+        caracter = texto[contador]
+        if caracter not in [" ", ",", ".", ":", ";", "!", "?", "'", "\"", "-", "(", ")", "[", "]", "{", "}", "_"]:
+            texto_limpio += caracter.lower()
+        contador += 1
+        
     return False
 
